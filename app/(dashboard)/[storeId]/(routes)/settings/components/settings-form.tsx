1903e5ac2029b78c91839a576dabeea1c1b925a6
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-06-26 15:01:55 +0700

    update progress

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Store } from "@prisma/client"
+
+interface SettingsFormProps {
+    initialData: Store
+}
+
+export const SettingsForm = () => {
+    return (
+        <div>
+            ini form setting
+        </div>
+    )
+}
+
