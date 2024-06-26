a78306b6d96abf3c3c4d9a5931edc533e4e5be38
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-06-26 22:48:22 +0700

    update progress

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,114 @@
-'use client'
+"use client";
 
-import { Store } from "@prisma/client"
+import * as z from "zod";
+import { useState } from "react";
+import axios from "axios"
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
-    initialData: Store
+  initialData: Store;
 }
 
-export const SettingsForm = () => {
-    return (
-        <div>
-            ini form setting
-        </div>
-    )
-}
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
+type SettingFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+
+    const params = useParams()
+    const router = useRouter()
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingFormValues) => {
+    try {
+        setLoading(true)
+        await axios.patch(`/api/stores/${params.storeId}`, data)
+        router.refresh()
+        toast.success("toko berhasil diupdate")
+    } catch (error) {
+        toast.error("Cek kembali data yang diinput")
+    } finally {
+        setLoading(false)
+    }
+  };
+
+  return (
+    <>
+    <AlertModal
+    isOpen={open}
+    onClose={() => setOpen(false)}
+    onConfirm={() => {}}
+    loading={loading}
+    />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Atur Toko" />
+        <Button
+        disabled={loading} 
+        variant="destructive" size="sm" onClick={() => setOpen(true)}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nama Toko"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            disabled={loading}
+            type="submit"
+          >
+            Save
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
